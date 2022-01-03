ec478b51746be77d865b45fa359f173f2fc0ff73
Mark Stefanovic <markstefanovic@hotmail.com>
2022-01-03 06:05:03 -0800

    debounce job log filter

--- src/main/kotlin/presentation/log/view/JobLogListView.kt
+++ src/main/kotlin/presentation/log/view/JobLogListView.kt
@@ -1,5 +1,6 @@
 package presentation.log.view
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
@@ -16,17 +17,31 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
 import presentation.log.bloc.JobLogEvents
 import presentation.log.bloc.JobLogState
 import presentation.shared.abbreviatedTimestampFormat
@@ -42,25 +57,64 @@ fun JobLogListView(
 
   val listState = rememberLazyListState()
 
+  val coroutineScope = rememberCoroutineScope()
+
+  val showScrollToTopButton by remember {
+    derivedStateOf {
+      listState.firstVisibleItemIndex > 0
+    }
+  }
+
+  var filter by remember { mutableStateOf(state.filter) }
+
+  LaunchedEffect("filter") {
+    snapshotFlow {
+      filter
+    }
+      .distinctUntilChanged()
+      .debounce(1000)
+      .collect {
+        events.setJobNameFilter(it)
+      }
+  }
+
   println("JobLogListView.state: $state")
 
   Column(
     modifier = Modifier.padding(6.dp)
   ) {
     Row {
+      AnimatedVisibility(visible = showScrollToTopButton) {
+        IconButton(
+          onClick = {
+            coroutineScope.launch {
+              listState.animateScrollToItem(index = 0)
+            }
+          }
+        ) {
+          Icon(
+            imageVector = Icons.Filled.KeyboardArrowUp,
+            contentDescription = "Scroll to Top",
+          )
+        }
+      }
+
       Button(
         onClick = events::refresh,
         colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray),
         border = BorderStroke(width = 1.dp, color = Color.White),
       ) {
-        Text("Refresh", fontWeight = FontWeight.ExtraBold, color = Color.LightGray)
+        Text(
+          text = "Refresh",
+          fontWeight = FontWeight.ExtraBold, color = Color.LightGray,
+        )
       }
 
       if (state.latestRefresh != null) {
         Spacer(Modifier.weight(1f))
 
         Text(
-          "Last Refresh: ${abbreviatedTimestampFormat.format(state.latestRefresh)}",
+          text = "Last Refresh: ${abbreviatedTimestampFormat.format(state.latestRefresh)}",
           modifier = Modifier.align(Alignment.CenterVertically),
         )
       }
@@ -69,9 +123,9 @@ fun JobLogListView(
     Spacer(Modifier.height(6.dp))
 
     TextField(
-      value = state.filter,
+      value = filter,
       onValueChange = { txt ->
-        events.setJobNameFilter(txt)
+        filter = txt
       },
       label = { Text("Job", modifier = Modifier.fillMaxHeight()) },
       maxLines = 1,
